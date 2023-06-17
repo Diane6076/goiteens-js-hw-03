4cbf98407a5af651e9a40172940166b0d890e497
diana <dskripka005@gmail.com>
2023-06-17 13:49:51 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const result= 5 + 5 + '5' ;
+console.log(result);
+console.log(typeof(result));
+
+const email = 'dsk000@wow' ;
+console.log(email.includes('@'));
+console.log(email.length);
+
+const oneOnlyMy = 'my';
+const twoOnlyName = 'name';
+const threeonlyIs = 'is';
+const fullName = oneOnlyMy + '' + twoOnlyName + '' +threeonlyIs + 'Viktor ';
+console.log(fullName);
+
+
+const userNme = 'diana';
+const payment = '300';
+const message = `Дякуемо,${userNme} ! До сплати  ${payment} гривень`;
+alert(message);
